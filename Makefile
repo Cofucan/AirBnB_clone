20ebac2310516a23734f85ad7d28552311316c67
Uche Ofia <coolucheofia@gmail.com>
2023-07-16 20:38:11 +0100

    feat: Create console program

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 .PHONY: executable
 
 executable:
-	chmod +x $(shell find . -name "*.py")
+	chmod +x *.py
+	chmod +x $(shell find ./models -name "*.py")
+	chmod +x $(shell find ./tests -name "*.py")
 
 pycodestyle:
 	pycodestyle $(shell find ./models -name "*.py")
---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines a cmd subclass that is an entry point to the commandline.
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """A simple commandline interpreter for HBNB."""
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line: str) -> bool:
+        """Enables exiting the program with Crtl-D"""
+        return True
+
+    def do_quit(self, line: str) -> bool:
+        """Quit command to exit the program"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
